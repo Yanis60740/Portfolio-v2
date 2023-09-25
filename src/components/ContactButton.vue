1709f7c8e5758eb7795190c658862a65f20b7029
Yanis <94384207+Yanis60740@users.noreply.github.com>
2023-09-25 17:43:24 +0200

    Add files via upload

--- src/components/ContactButton.vue
+++ src/components/ContactButton.vue
@@ -1,5 +1,9 @@
 <template>
-    <div></div>
+    <div>
+        <button class="contactButton">
+            <span class="contactButton__text">GET IN TOUCH</span>
+        </button>
+    </div>
 </template>
 
 <script>
@@ -9,3 +13,19 @@ export default {
     }
 }
 </script>
+
+<style lang="scss" scoped>
+@import "../css/variables.scss";
+.contactButton{
+    padding: 16px 40px;
+    background: $gradient-color;
+    border-radius: 56px;
+    border: 1px solid #343434;
+    &__text{
+        font-family: $open-sans;
+        font-size: $small-size;
+        font-weight: 700;
+        color: $primary-color;
+    }
+}
+</style>
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,11 +1,138 @@
 <template>
-    <div></div>
+  <div class="navContainer">
+    <div class="navBar">
+      <div class="navBar__logo">YANIS ABID</div>
+      <div class="navBar__links">
+        <a
+          v-for="section in sections"
+          :key="section.name"
+          :href="formatHref(section.name)"
+        >
+          {{ section.name.toUpperCase() }}
+        </a>
+        <a href="#contact-me">CONTACT ME</a>
+        <div class="navBar__switch">
+          <div class="navBar__switch__circle"></div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import jsonData from "../data.json";
+import gsap from "gsap";
 export default {
-    name: 'NavBar',
-    components: {
-    }
-}
+  name: "NavBar",
+  components: {},
+  data() {
+    return {
+      sections: jsonData.sections,
+      isSwitched: false,
+    };
+  },
+  mounted() {
+    this.setupSwitchAnimation();
+  },
+  methods: {
+    formatHref(name) {
+      return "#" + name.toLowerCase().split(" ").join("-");
+    },
+    setupSwitchAnimation() {
+      const switchButton = this.$el.querySelector(".navBar__switch");
+      const switchCircle = this.$el.querySelector(".navBar__switch__circle");
+        const duree = 0.4;
+      switchButton.addEventListener("click", () => {
+        this.isSwitched = !this.isSwitched;
+
+        if (this.isSwitched) {
+            console.log("switched");
+          gsap.to(switchCircle, {
+            x: "15px",
+            duration: duree,
+            ease : "power2.inOut",
+            background: "#343434"
+            });
+            gsap.to(switchButton, {
+                background: "linear-gradient(to bottom, #CDB4BF, #F2F2F2)",
+                duration: duree,
+                ease : "power2.inOut"
+            });
+        } else {
+            console.log("no switch");
+            gsap.to(switchCircle, {
+            x: "1px",
+            duration: duree,
+            ease : "power2.inOut",
+            background: "linear-gradient(to bottom, #CDB4BF, #F2F2F2)"
+            });
+            gsap.to(switchButton, {
+                background: "#343434",
+                duration: duree,
+                ease : "power2.inOut"
+            });
+            
+        }
+      });
+    },
+  },
+};
 </script>
+
+<style lang="scss" scoped>
+@import "../css/variables.scss";
+.navContainer {
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  position: fixed;
+  padding: 16px 40px;
+  box-sizing: border-box;
+  top: 0;
+  left: 0;
+  border-bottom: 1px solid #323232;
+}
+.navBar {
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  align-items: center;
+  font-size: $small-size;
+  max-width: 1840px;
+  
+  & a {
+    font-family: $open-sans;
+    color: $primary-color;
+    font-weight: 700;
+  }
+  &__logo {
+    color: $primary-color;
+    font-family: $dela-gothic;
+    font-weight: 400;
+  }
+  &__links {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    width: 1117px;
+  }
+  &__switch {
+    width: 45px;
+    height: 20px;
+    background: $primary-color;
+    position: relative;
+    border-radius: 15px;
+    cursor: pointer;
+    &__circle {
+      width: 18px;
+      height: 18px;
+      background: $gradient-color;
+      border-radius: 50%;
+      position: absolute;
+      top: 1px;
+      left: 1px;
+    }
+  }
+}
+</style>
