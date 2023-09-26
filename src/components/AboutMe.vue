7c03470939e33ee3727867a1c590d6c3517d3545
Lyman Abid <merge.tool>
2023-09-26 12:09:16 +0200

    avancée dans le about me

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -1,25 +1,101 @@
 <template>
-    <div class="AboutMe">
-            
+  <div class="aboutMe">
+    <div class="aboutMe__title">
+      <h2>{{ section.name.toUpperCase() }}</h2>
     </div>
+    <div class="aboutMe__box">
+      <div class="aboutMe__content">
+        <div class="aboutMe__content__left">
+          <div></div>
+        </div>
+        <div class="aboutMe__content__right">
+            <div v-for="accroche in section.accroche" :key="accroche">
+            <h3>{{ accroche }}</h3>
+          </div>
+          <div v-for="paragraph in section.content" :key="paragraph">
+            <p>{{ paragraph }}</p>
+          </div>
+          <div>
+            <img :src="iconPath" alt="icon_localisation">
+            
+            <span>{{ section.localisation }}</span>
+        </div>
+          <div><ContactButton /></div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 import jsonData from "../data.json";
 import ContactButton from "./ContactButton.vue";
 export default {
-    name: 'AboutMe',
-    components: {
-        ContactButton,
-    },
-    data() {
-        return {
-            title: jsonData.title,
-        };
-    },
-}
+  name: "AboutMe",
+  components: {
+    ContactButton,
+  },
+  data() {
+    return {
+      title: jsonData.title,
+      section: jsonData.sections[0],
+      iconPath: require(`@/assets/icones/${jsonData.sections[0].icon}`)
+    };
+  },
+};
 </script>
 
 <style lang="scss" scoped>
 @import "../css/variables.scss";
+
+.aboutMe {
+  &__title {
+    padding: 20px 0;
+    box-sizing: border-box;
+    border-bottom: 1px solid $primary-color;
+  }
+  &__content {
+    display: flex;
+    justify-content: space-between;
+    width: 70%;
+    &__left {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      height: 100%;
+      width: 40%;
+      & div {
+        background-image: url("../assets/images/illustration.svg");
+        border-radius: 50%;
+        aspect-ratio: 1/1;
+        background-repeat: no-repeat;
+        background-position: center;
+        background-size: cover;
+        width: 320px;
+      }
+    }
+    &__right {
+      background-color: aqua;
+      width: 40%;
+      color: $primary-color;
+      & h3{
+        text-align: left;
+        font-family: $dela-gothic;
+        font-weight: 400;
+        font-size: $small-size;
+      }
+      & p {
+        text-align: left;
+      }
+    }
+    
+    
+  }
+}
+
+.aboutMe__box {
+  display: flex;
+  justify-content: center;
+  width: 100%;
+}
 </style>
--- src/components/ContactButton.vue
+++ src/components/ContactButton.vue
@@ -1,31 +1,109 @@
 <template>
-    <div>
-        <button class="contactButton">
-            <span class="contactButton__text">GET IN TOUCH</span>
-        </button>
-    </div>
+  <div>
+    <button class="contactButton"  @mouseover="hover" @mouseleave="unhover">
+      <div class="contactButton__bg" ref="contactBg"></div>
+      <div class="contactButton__text">
+        <span class="firstSpan" ref="firstSp">GET IN TOUCH</span>
+        <span class="secondSpan" ref="secondSp">GET IN TOUCH</span>
+      </div>
+    </button>
+  </div>
 </template>
 
 <script>
+import gsap from "gsap";
 export default {
-    name: 'ContactButton',
-    components: {
-    }
-}
+  name: "ContactButton",
+  data() {
+    return {
+        duration: 0.7,
+    };
+  },
+  methods: {
+    hover() {
+        gsap.to(this.$refs.contactBg, {
+        top: 0,
+        duration: this.duration,
+        // delay: 0.17,
+        ease: "power2.out",
+      });
+      gsap.to(this.$refs.firstSp, {
+        translateY: "-110%",
+        duration: this.duration,
+        // delay: 0.17,
+        opacity: 0,
+        ease: "power2.out",
+      });
+      gsap.to(this.$refs.secondSp, {
+        translateY: "0%",
+        duration: this.duration,
+        opacity: 1,
+        ease: "power2.out",
+      });
+      
+    },
+    unhover() {
+      gsap.to(this.$refs.contactBg, {
+        top: "100%",
+        duration: this.duration,
+        // delay: 0.17,
+        opacity: 1,
+        ease: "power2.out",
+      });
+      gsap.to(this.$refs.firstSp, {
+        translateY: "0%",
+        duration: this.duration,
+        // delay: 0.17,
+        opacity: 1,
+        ease: "power2.out",
+      });
+      gsap.to(this.$refs.secondSp, {
+        translateY: "110%",
+        duration: this.duration,
+        // delay: 0.26,
+        opacity: 0,
+        ease: "power2.out",
+      });
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
 @import "../css/variables.scss";
-.contactButton{
-    padding: 16px 40px;
-    background: $gradient-color;
-    border-radius: 56px;
-    border: 1px solid #343434;
-    &__text{
-        font-family: $open-sans;
-        font-size: $small-size;
-        font-weight: 700;
-        color: $primary-color;
-    }
+.contactButton {
+  padding: 16px 40px;
+  background: $gradient-color;
+  border-radius: 56px;
+  border: 1px solid #343434;
+  overflow: hidden;
+  position: relative;
+  &__bg{
+    background: $primary-color;
+    width: 251.05px;
+    left: 0;
+    top: 100%;
+    height: 66.4px;
+    position: absolute;
+}
+  &__text {
+    font-family: $open-sans;
+    font-size: $small-size;
+    font-weight: 700;
+    color: $primary-color;
+    display: flex;
+    flex-direction: column;
+  }
+  &__before {
+  }
+  &__after {
+  }
+}
+
+.secondSpan{
+    position: absolute;
+    transform: translateY(110%);
+    color: $secondary-color;
+    opacity: 0;
 }
 </style>
